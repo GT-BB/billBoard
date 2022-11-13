6759f5332bb58e556ef23e87cab12fe71537fe51
Raey Ayalew <raeyayalew@gmail.com>
2022-11-13 16:53:53 -0500

    Made changes to allow hitting the form button to actually change set1.json making it dynamic instead of static woooooooooooooo

--- Back/routes/index.js
+++ Back/routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-/*const alertBox = function (req, res, next) {
-  alert("You have successfully submitted your data!");
-  next()
-}
-
-router.use(alertBox) */
+var fs = require('fs')
 
 router.post('/', function(req, res, next) {
   var data = req.body;
@@ -21,10 +15,33 @@ router.post('/', function(req, res, next) {
   const sqlite3 = require('sqlite3').verbose();
   let db = new sqlite3.Database("data.db");
 
-  db.run('INSERT INTO entries VALUES (NULL, ?, datetime("now", "localtime"), ?, ?, ?, ?)', [Procedure, Cost, State, Gender, Age]);
+  console.log(db.run('INSERT INTO entries VALUES (NULL, ?, datetime("now", "localtime"), ?, ?, ?, ?)', [Procedure, Cost, State, Gender, Age]));
 
-  db.close();
-  bread = fetch("http://127.0.0.1:8000/api/users") // Might remove if doesn't work
+  var sql = "SELECT * FROM procedures INNER JOIN entries ON procedures.proc_id = entries.proc_id;"
+  var params = []
+  console.log("reached here")
+
+  db.all(sql, params, (err, rows) => {
+      /*if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }*/
+
+      /*res.json({
+          "message":"success",
+          "data":rows
+      })*/
+
+      dict = {"data": rows}
+      console.log(dict)
+      var dictstring = JSON.stringify(dict)
+      fs.writeFile("../Front/jsonFiles/set1.json", dictstring, function(err, result) {
+          if(err) console.log('error', err);
+      });
+
+    });
+
+  db.close()
   res.redirect("http://127.0.0.1:5501/Front/website/html/post.html")
   
 });
